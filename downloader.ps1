cae817806f07716c5d77305de5658922948bc801
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2024-12-23 18:54:59 +0200

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -3,5 +3,7 @@ $outfile = "$($env:TEMP)/UpdateManager.zip"
 $folder = "$($env:TEMP)/UpdateManager"
 if (-not (Test-Path $outfile)){Invoke-WebRequest -Uri $uri -OutFile $outfile}
 if (-not (Test-Path $folder)){Expand-Archive $outfile -DestinationPath $folder}
+$PathParent = $PWD.Path
 Set-Location $folder
 ."$($folder)\patchsync.bat" -c updatedb -b 11.3.2440.01
+Set-Location $PathParent
