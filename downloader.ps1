ce7d375b282ae31708f8ffc7e7dcbbabd94e0d30
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2024-12-23 12:51:21 +0200

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -5,4 +5,3 @@ if (-not (Test-Path $outfile)){Invoke-WebRequest -Uri $uri -OutFile $outfile}
 if (-not (Test-Path $folder)){Expand-Archive $outfile -DestinationPath $folder}
 Set-Location $folder
 ."$($folder)\patchsync.bat" -c updatedb -b 11.3.2440.01
-Compress-Archive -Path updatedb -DestinationPath updatedb.zip
