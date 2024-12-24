bfde3a6dcbdd9161e73abf6a007e2b0bfc4d120a
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2024-12-24 09:17:12 +0200

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -4,6 +4,11 @@ $folder = "$($env:TEMP)/UpdateManager"
 if (-not (Test-Path $outfile)){Invoke-WebRequest -Uri $uri -OutFile $outfile}
 if (-not (Test-Path $folder)){Expand-Archive $outfile -DestinationPath $folder}
 $PathParent = $PWD.Path
+write-output "location is $($PathParent)"
 Set-Location $folder
 ."$($folder)\patchsync.bat" -c updatedb -b 11.3.2440.01
-Compress-Archive -Path "updatedb" -DestinationPath "$($PathParent)\updatedb.zip"
+dir
+Compress-Archive -Path updatedb -DestinationPath updatedb.zip
+dir
+Copy-Item updatedb.zip $PathParent
+write-output "location is $($PathParent)"
