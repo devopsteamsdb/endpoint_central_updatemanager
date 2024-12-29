a846e1eb7ca4753259c23e4a2044d75a7968045c
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2024-12-29 08:03:08 +0200

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -1,6 +1,7 @@
 $uri = "https://patchdb.manageengine.com/crs/dmz/UpdateManager.zip"
 $outfile = "$($env:TEMP)/UpdateManager.zip"
 $folder = "$($env:TEMP)/UpdateManager"
+$date = (Get-Date).ToString("yyyy_MM_dd_hh_mm_ss")
 if (-not (Test-Path $outfile)){Invoke-WebRequest -Uri $uri -OutFile $outfile}
 if (-not (Test-Path $folder)){Expand-Archive $outfile -DestinationPath $folder}
 $PathParent = $PWD.Path
@@ -8,7 +9,7 @@ write-output "location is $($PathParent)"
 Set-Location $folder
 ."$($folder)\patchsync.bat" -c updatedb -b 11.3.2440.01
 dir
-Compress-Archive -Path updatedb -DestinationPath updatedb.zip
+Compress-Archive -Path updatedb -DestinationPath "updatedb_$($date).zip"
 dir
-Copy-Item updatedb.zip $PathParent
+Copy-Item "updatedb_$($date).zip" $PathParent
 write-output "location is $($PathParent)"
