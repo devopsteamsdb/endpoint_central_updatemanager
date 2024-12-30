147e94abba82bd75ae402df9916eda67bac94265
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2024-12-30 11:31:43 +0200

    added linux updates

--- downloader.ps1
+++ downloader.ps1
@@ -1,3 +1,5 @@
+# this is the url for guideline https://www.manageengine.com/products/desktop-central/help/configuring_desktop_central/patch_management_for_closed_network.html
+
 $uri = "https://patchdb.manageengine.com/crs/dmz/UpdateManager.zip"
 $outfile = "$($env:TEMP)/UpdateManager.zip"
 $folder = "$($env:TEMP)/UpdateManager"
@@ -7,7 +9,7 @@ if (-not (Test-Path $folder)){Expand-Archive $outfile -DestinationPath $folder}
 $PathParent = $PWD.Path
 write-output "location is $($PathParent)"
 Set-Location $folder
-."$($folder)\patchsync.bat" -c updatedb -b 11.3.2440.01
+."$($folder)\patchsync.bat" -c updatedb -i linux -b 11.3.2440.01
 dir
 Compress-Archive -Path updatedb -DestinationPath "updatedb_$($date).zip"
 dir
